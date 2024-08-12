8436ee95ac1a18565b758f598b67147ae5093eb0
mudkipdev <mudkip@mudkip.dev>
2024-08-11 19:43:21 -0600

    modify message

--- src/main/java/jam/game/Effect.java
+++ src/main/java/jam/game/Effect.java
@@ -26,7 +26,10 @@ import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
 public enum Effect implements Titleable {
-    INK_BLASTER(Material.WHITE_CANDLE, "<yellow><bold>[INFO]<reset> <gray>An <yellow><bold>Ink Blaster<reset><gray> has spawned in a <light_purple>random<gray> spot!\n<yellow><bold>[INFO]<reset> <gray>Shoot <dark_gray>ink<gray> to change the <red>color<gray> of blocks!") {
+    INK_BLASTER(
+            Material.WHITE_CANDLE,
+            "<gray>An <yellow><bold>Ink Blaster<reset><gray> has spawned in a <light_purple>random<gray> spot!\n<gray>Shoot ink to change the colors of blocks!"
+    ) {
         private static final Set<Point> POINTS = Sphere.getBlocksInSphere(2.0D);
         private static final double SPREAD = 0.1D;
         private static final double BULLETS = 10.0D;
